42a7b000a773f039a9866f7d5d206a10942694dd
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-18 01:58:43 +0500

    resolved react-spring installation issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './moon.png'
+import './cat.gif'
+import './land.png'
+import {Parallax, ParallaxLayer} from "react-spring/addons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src="moon.png" alt=""/>
+        </div>
+    );
 }
 
 export default App;
