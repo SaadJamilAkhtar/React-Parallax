f2d2bc8a6a5cae88432062e61fc09fe2359fe340
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-18 12:37:15 +0500

    initial content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './moon.png'
 import './cat.gif'
 import './land.png'
-import {Parallax, ParallaxLayer} from "react-spring/addons";
+import './App.css'
+import {Parallax, ParallaxLayer} from "@react-spring/parallax";
 
 function App() {
     return (
-        <div className="App">
-            <img src="moon.png" alt=""/>
+        <div className={'Main'}>
+            <Parallax pages={4}>
+                <ParallaxLayer>
+                    <div className="centerContent">
+                        <h2>Welcome to My Web</h2>
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer>
+                    <div className="centerContent">
+                        <h2>The Parallax Effect</h2>
+                    </div>
+
+                </ParallaxLayer>
+            </Parallax>
         </div>
     );
 }
