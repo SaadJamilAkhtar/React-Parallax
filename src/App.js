79c6fd1dac87bce0865da182cc7faee51a532273
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-18 12:46:10 +0500

    added initial content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './moon.png'
-import './cat.gif'
-import './land.png'
+import moon from './moon.png'
+import cat from './cat.gif'
+import land from './land.png'
 import './App.css'
 import {Parallax, ParallaxLayer} from "@react-spring/parallax";
 
@@ -8,16 +8,32 @@ function App() {
     return (
         <div className={'Main'}>
             <Parallax pages={4}>
-                <ParallaxLayer>
+                <ParallaxLayer
+                    speed={1}
+                    factor={2}
+                    style={{
+                        backgroundImage: `url(${moon})`,
+                        backgroundSize: 'cover'
+                    }}
+                >
                     <div className="centerContent">
                         <h2>Welcome to My Web</h2>
                     </div>
+                </ParallaxLayer>
+                <ParallaxLayer
+                    offset={1}
+                    speed={1}
+                    factor={4}
+                    style={{
+                        backgroundImage: `url(${land})`,
+                        backgroundSize: 'cover'
+                    }}
+                >
                 </ParallaxLayer>
                 <ParallaxLayer>
                     <div className="centerContent">
                         <h2>The Parallax Effect</h2>
                     </div>
-
                 </ParallaxLayer>
             </Parallax>
         </div>
